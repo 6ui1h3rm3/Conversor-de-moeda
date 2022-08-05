82258fc7a2cb3c1290d0e46d6a70c838f09b5010
Guilherme Jose <need_88g@hotmail.com>
2022-08-05 10:56:05 -0300

    Update Conversor de moeda.py

--- Conversor de moeda.py
+++ Conversor de moeda.py
@@ -2,4 +2,4 @@ real = float(input('Quanto dinheiro você tem na carteira? R$'))
 dolar = real / 5.23
 euro = real / 5.34
 print('Com R$ {:.2f} você pode comprar U$ {:.2f}'.format(real, dolar))
-print('Com R$ {:.2f} você pode comprar EUR {:.2f}'.format(real, euro))
+print('Com R$ {:.2f} você pode comprar €$ {:.2f}'.format(real, euro))
